da4034fefedfced8b7fdec90f9216d4dd84fe2c4
Jean090504 <jc090504@gmail.com>
2026-03-05 12:52:05 -0300

    Adicionando readme

--- server.js
+++ server.js
@@ -9,5 +9,5 @@ const app = require('./src/app');
 const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log(`🚀 Servidor iniciado em: http://localhost:${PORT}`);
-});
+    console.log(`🚀 Servidor iniciado em: http://localhost:${PORT}`)
+})
