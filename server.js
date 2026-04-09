f3594daf9251e20e58e23a7cb6e71ae8989c9501
Jean090504 <jc090504@gmail.com>
2026-04-09 17:22:26 -0300

    Adicionando port

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ const app = require('./src/app');
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`🚀 Servidor rodando na porta ${PORT}`)
 })
