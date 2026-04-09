ab0e0d13707881f3e81358c70deb64ad9b504420
Jean090504 <jc090504@gmail.com>
2026-04-09 17:03:58 -0300

    Adicionando hemlmet

--- src/app.js
+++ src/app.js
@@ -2,19 +2,27 @@
 * Objetivo: Desenvolver uma API de Gestão de Folha de Pagamento
 * Data: 05/03/2026
 * Autor: Jean Costa
-* Versão 1.0
+* Versão 1.1
 ********************************************************************************************************************************************/
 
-const express = require('express');
-const cors = require('cors');
-const funcionarioRoutes = require('./routes/funcionarioRoutes');
+const express = require('express')
+const cors = require('cors')
+const funcionarioRoutes = require('./routes/funcionarioRoutes')
 
-const app = express();
+const app = express()
 
-app.use(cors()); 
-app.use(express.json());
+const corsOptions = {
+    origin: "*",
+    methods: "GET", 
+    allowedHeaders: ["Content-Type", "Authorization"], 
+    credentials: true 
+}
+
+app.use(cors(corsOptions))
+app.use(helmet())
+app.use(express.json())
 
 //Monta a rota /funcionario
-app.use('/funcionario', funcionarioRoutes);
+app.use('/funcionario', funcionarioRoutes)
 
-module.exports = app;
+module.exports = app
