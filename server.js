7826f03ea8cf76c6625af9ca0ba76e3c1b113185
Jean090504 <jc090504@gmail.com>
2026-04-09 17:05:40 -0300

    Arrumando porta

--- server.js
+++ server.js
@@ -6,8 +6,9 @@
 ********************************************************************************************************************************************/
 
 const app = require('./src/app');
-const PORT = 3000;
+
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-    console.log(`🚀 Servidor iniciado em: http://localhost:${PORT}`)
+    console.log(`🚀 Servidor rodando na porta ${PORT}`)
 })
